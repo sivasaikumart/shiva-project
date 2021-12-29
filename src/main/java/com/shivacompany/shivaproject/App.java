836bb1ef44dff75dd15b3fbea641ddd9ec1aaf89
sivasaikumart <62865119+sivasaikumart@users.noreply.github.com>
2021-12-29 21:48:25 +0530

    added first commit

---
+++ src/main/java/com/shivacompany/shivaproject/App.java
@@ -0,0 +1,13 @@
+package com.shivacompany.shivaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
